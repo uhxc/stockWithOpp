0cfb00b39e45f0b5206e3f6d6c4e55fa3d63720a
migue <migue@DESKTOP-5U5RGJB>
2023-06-21 17:25:57 -0300

    Add project files.

---
+++ stockWithOpp/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stockWithOpp
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public override string ToString()
+        {
+            return $"Dados do produto: {Name}, {Price:f2}, {Quantity} unidades, total: {ValorEmEstoque():f2}";
+        }
+
+        public double ValorEmEstoque()
+        {
+            return Price * Quantity;
+        }
+        public void AdicionarProduto()
+        {
+            Console.WriteLine("digite a quantade a ser adicionada em estoque");
+            int a = int.Parse(Console.ReadLine());
+            Quantity += a;
+        }
+        public void RemoverProduto()
+        {
+            Console.WriteLine("digite a quantade a ser removida de estoque");
+            int b = int.Parse(Console.ReadLine());
+            Quantity -= b;
+        }
+
+    }
+}
---
+++ stockWithOpp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace stockWithOpp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Tv = new Product();
+            Console.WriteLine("escreva o nome do produto");
+            Tv.Name = Console.ReadLine();
+            Console.WriteLine("escreva o preco do produto");
+            Tv.Price = double.Parse(Console.ReadLine());
+            Console.WriteLine("escreva a quantidade do produto");
+            Tv.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Tv);
+            
+            
+            Tv.AdicionarProduto();
+            Console.WriteLine(Tv);
+
+            
+            Tv.RemoverProduto();
+            Console.WriteLine(Tv);
+        }
+    }
+}
